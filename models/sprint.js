f2ca01c798189fa336a4f1e7e244301ede0ab2c6
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2022-08-19 22:41:36 -0300

    Added sprint model

---
+++ models/sprint.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const sprintSchema = new mongoose.Schema({
+    title: {
+        required: true,
+        type: String,
+        unique: true
+    },
+    description: {
+        type: String,
+        default: null
+    },
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+        default: null
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    creation: {
+        type: Date,
+        default: Date.now()
+    },
+    start: {
+        type: Date,
+        default: null
+    },
+    deadline: {
+        type: Date,
+        default: null
+    },
+    end: {
+        type: Date,
+        default: null
+    },
+    labels: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Label'
+        }
+    ]
+})
+
+module.exports = mongoose.model('Sprint', sprintSchema)
