1ca66c9455964e507ed89f69f93a4e2df3b26369
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2022-08-25 00:16:51 -0300

    Added Cors Configuration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const mongoString = process.env.DATABASE_URL;
 const baseApiRoute = '/api';
 const authenticationRouter = require('./routes/authentication')
@@ -22,7 +23,10 @@ database.once('connected', () => {
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
+.use(cors({
+    origin: '*'
+}));
 
 app.listen(3000, () => {
     console.log('Server Started at ${3000}')
